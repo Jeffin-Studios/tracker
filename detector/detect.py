98daeaddecc6a82cfa851d6841a355d40026b02d
Richard Bao <richardbao419@gmail.com>
2018-09-17 18:14:54 -0700

    edit

--- detector/detect.py
+++ detector/detect.py
@@ -16,10 +16,11 @@ CWD_PATH = os.getcwd()
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
 MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
 PATH_TO_CKPT = os.path.join(CWD_PATH, 'object_detection', MODEL_NAME, 'frozen_inference_graph.pb')
-# PATH_TO_CKPT = os.path.join(CWD_PATH, 'frozen_inference_graph.pb')
+# PATH_TO_CKPT = os.path.join(CWD_PATH, retrained_models, Model1, 'graph.pb')
 
 # List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = os.path.join(CWD_PATH, 'object_detection', 'data', 'mscoco_label_map.pbtxt')
+# PATH_TO_LABELS = os.path.join(CWD_PATH, retrained_models, Model1, 'labels.pbtxt')
 
 NUM_CLASSES = 90
 
